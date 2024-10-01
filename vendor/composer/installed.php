c33f79628a1f7d3278ab529fa003f097c8fa8741
Rianodji Dicard <dicardrianodji@gmail.com>
2024-10-01 02:35:45 +0200

    ajout de la licence

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rianodji/helloworld',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c869e53b97e8117b2009b5df6fdadb3427fb0dcf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rianodji/helloworld' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c869e53b97e8117b2009b5df6fdadb3427fb0dcf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
